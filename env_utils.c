933d9d7e01bc4ecfd40b856870afd432fbe94c61
tizihoxha <tizihoxha@gmail.com>
2022-07-26 03:41:02 -0700

    added

--- env_utils.c
+++ env_utils.c
@@ -1,32 +1,34 @@
 #include "shell.h"
+
 /**
 *
 *
 *
 */
-int my_strncmp(const char *s1, const char *s2, size_t n)
-int _str_n_cmp(char *s1, char *s2, int n)
+
+int _str_n_cmp(const char *s1, const char *s2, size_t n)
 {
 	char c1;
 	char c2;
-
+	
 	while (n)
 	{
 		c1 = *s1++;
 		c2 = *s2++;
 		if (c1 == '\0' || c1 != c2)
-		  {
-		    if (c1 > c2)
-		      return (1);
-		    else
-		      {
-			if (c1 < c2)
-			  return (-1);
+		{
+			if (c1 > c2)
+				return (1);
 			else
-			  return (0);
-		      }
-		  }
+			{
+				if (c1 < c2)
+					return (-1);
+				else
+					return (0);
+			}
+		}
 		n--;
+	}
 }
 
 /**
@@ -38,7 +40,7 @@ int _str_n_cmp(char *s1, char *s2, int n)
 void _printenv(char **environ)
 {
 	int i;
-
+	
 	for (i = 0; environ[i] ; i++)
 		_putchar(environ[i]);
 }
@@ -56,20 +58,19 @@ char *_getenv(const char *name)
 
 for (i = 0; environ[i] != NULL; i++)
 {
-    status = 1;
-    for (j = 0; environ[i][j] != '='; j++)
-    {
-        if (name[j] != environ[i][j])
-        {
-            status = 0;
-            break;
-        }
-    }
-
-    if (status)
-    {
-        return (&environ[i][j + 1]);
-    }
+	status = 1;
+	for (j = 0; environ[i][j] != '='; j++)
+	{
+		if (name[j] != environ[i][j])
+		{
+			status = 0;
+			break;
+		}
+	}
+	if (status)
+	{
+		return (&environ[i][j + 1]);
+	}
 }
 return (NULL);
 }
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <strings.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#in
+
+#endif
