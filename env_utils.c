6bc1f14a9570778a78e32a39baba6e1f415119f3
FatjonMileti <tonimileti@yahoo.com>
2022-07-26 04:13:34 -0700

    ...

--- env_utils.c
+++ env_utils.c
@@ -51,26 +51,12 @@ void _printenv(char **environ)
 *
 */
 
-char *_getenv(const char *name)
+char *_getenv(int argc, char *argv[], char *envp[])
 {
-    int i, j;
-    int status;
-
-for (i = 0; environ[i] != NULL; i++)
-{
-	status = 1;
-	for (j = 0; environ[i][j] != '='; j++)
-	{
-		if (name[j] != environ[i][j])
-		{
-			status = 0;
-			break;
-		}
-	}
-	if (status)
-	{
-		return (&environ[i][j + 1]);
-	}
-}
-return (NULL);
+    int i;
+    
+    for (i = 0; envp[i] != NULL; i++)
+	    printf("\n%s", envp[i]);
+    getchar();
+    return (0);
 }
