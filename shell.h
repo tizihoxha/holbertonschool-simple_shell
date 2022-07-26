8e8dc63c732bccb3a7621e9a400d3f97ee9dde3e
tizihoxha <tizihoxha@gmail.com>
2022-07-26 03:54:56 -0700

    added

--- shell.h
+++ shell.h
@@ -6,6 +6,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#in
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
+
+extern char **environ;
+int _strlen(char *s);
+int _strcmp(char *strg1, char *strg2);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+int _str_n_cmp(const char *s1, const char *s2, size_t n);
+void _printenv(char **environ);
+char *_getenv(const char *name);
 
 #endif
