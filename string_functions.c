b2963c0823a28a181449e0fa1145fa5315bb318b
tizihoxha <tizihoxha@gmail.com>
2022-08-02 12:14:30 -0700

    added

--- string_functions.c
+++ string_functions.c
@@ -9,7 +9,7 @@
 
 int _strcmp(char *strg1, char *strg2)
 {
-	while( ( *strg1 != '\0' && *strg2 != '\0' ) && *strg1 == *strg2 )
+	while(( *strg1 != '\0' && *strg2 != '\0' ) && *strg1 == *strg)
     {
         strg1++;
         strg2++;
