4ad6d7779c7dac8d84628fb9aea485c138656e91
tizihoxha <tizihoxha@gmail.com>
2022-08-03 12:57:01 -0700

    added

--- shell.c
+++ shell.c
@@ -54,11 +54,10 @@ int execute(char *cmd_arr[])
 	execute_path = command_path(cmd);
 	if (execute_path == NULL)
 	{
-		/**write(2, name, _strlen(name));
+		write(2, name, _strlen(name));
 		write(2, ": ", 2);
 		write(2, cmd, _strlen(cmd));
-		write(2, ": not found\n", 12);*/
-		perror("Command not found");
+		write(2, ": not found\n", 12);
 
 		return (3);
 	}
